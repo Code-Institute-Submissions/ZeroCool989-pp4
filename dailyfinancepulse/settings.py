2404850a941af9746a05ef896c081c68b7a9125d
ZeroCool989 <almirdumisic@gmail.com>
2024-01-26 21:14:18 +0000

    Set DEBUG to False and added X_FRAME_OPTIONS='SAMEORIGIN' for Heroku deployment

--- dailyfinancepulse/settings.py
+++ dailyfinancepulse/settings.py
@@ -30,7 +30,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS= 'SAMEORIGIN'
+
+
 
 ALLOWED_HOSTS = [
     '8000-zerocool989-pp4-5swibsdgig0.ws-eu107.gitpod.io',
